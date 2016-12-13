849e758b4d6272337f1bf8e1a287f3279aac668c
oussama sarhraoui <sarhraoui.oussama@gmail.com>
2016-12-13 11:42:23 +0000

    Add insert, findWord methods for the Db class

--- src/main/java/com/dsearch/db/Db.java
+++ src/main/java/com/dsearch/db/Db.java
@@ -7,9 +7,17 @@
 
 package com.dsearch.db;
 
+import java.util.HashMap;
 
 public class Db {
 	private String dbName = null;
+	private HashMap<String, Word> records;
+	/**
+	 * This the number of supported words
+	 * there is at most one quarter of a million english words
+	 * see: https://en.oxforddictionaries.com/explore/how-many-words-are-there-in-the-english-language 
+	 */
+	private final static int DB_SIZE = 1000000;
 	/**
 	 * A constructor for the DB.
 	 * <p>
@@ -19,18 +27,10 @@ public class Db {
 	public Db(String dbName) {
 		super();
 		this.setDbName(dbName);
-	}
+		this.records = new HashMap<>(DB_SIZE);
 
-	/**
-	 * 
-	 * @param nextCommand
-	 * @return
-	 */
-	public String search(String nextCommand) {
-		return "";
-		// TODO Auto-generated method stub
-		
 	}
+	
 
 	public String getDbName() {
 		return dbName;
@@ -38,5 +38,43 @@ public class Db {
 
 	public void setDbName(String dbName) {
 		this.dbName = dbName;
+	}
+
+	public boolean wordExists(String word) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	public Word findWord(String word) {
+		return this.records.get(word);
+	}
+	
+	/**
+	 * Insert a  word or update an existing one to the records Hashmap.
+	 * @param word	a word to be inserted.
+	 * @param pos	Specify the position of the word.  
+	 * @param filename Specify the file where the word is found. 
+	 */
+	public void insert(Word word, int pos, String filename) {
+		Word oldWord = this.records.get(word.getValue()); 
+		if (oldWord != null) {
+			oldWord.updateFilepath(filename, pos);
+			this.records.put(oldWord.getValue(), oldWord);
+		} else {
+			Word newWord = new Word();
+			newWord.setValue(word.getValue());
+			newWord.updateFilepath(filename, pos);
+			this.records.put(newWord.getValue(), word);
+		}
+		
+	}
+	public void keys() {
+		for (String key: this.records.keySet()){
+			System.out.println(key);
+		}; 
+	}
+	public void values() {
+		for (Word word: this.records.values()){
+			System.out.println(word.getFilepaths());
+		}; 
 	}
 }
